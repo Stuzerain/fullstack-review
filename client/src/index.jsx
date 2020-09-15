d844a12cea1fd03cb329e7ddbc3d406c5eba4025
Stuzerain <shanford3@gmail.com>
2020-09-14 20:09:31 -0700

    Added ability for server to receive POST requests from client using body-parser

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,22 +7,27 @@ import RepoList from './components/RepoList.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       repos: []
     }
 
   }
 
-  search (term) {
+  search(term) {
     console.log(`${term} was searched`);
     // TODO
+    $.post('http://localhost:1128/repos', {
+      username: term,
+      test: 'woo'
+    })
+      .done(console.log('Just sent a POST request to the server! The term was ', term))
   }
 
-  render () {
+  render() {
     return (<div>
       <h1>Github Fetcher</h1>
-      <RepoList repos={this.state.repos}/>
-      <Search onSearch={this.search.bind(this)}/>
+      <RepoList repos={this.state.repos} />
+      <Search onSearch={this.search.bind(this)} />
     </div>)
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,13 +1,30 @@
 const express = require('express');
 let app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client/dist'));
+
+var whitelist = ['http://localhost:1128']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  console.log('req.body = ', req.body);
+  res.end();
 });
 
 app.get('/repos', function (req, res) {
